57c0f5e0b71f0d8998b3d2ccdb908986a25c7d58
Lucas Leifheit <43593024+lnardon@users.noreply.github.com>
2019-05-03 00:26:03 -0300

    Update Palindromes.js

--- javascript/Palindromes.js
+++ javascript/Palindromes.js
@@ -1,1 +1,11 @@
+// This algorithm check if a word is Palindrome or not
 
+function isPalim(word){
+  let og = word;
+  let newWord = "";
+  for (let i = 0; i<word.length; i++ ){
+    newWord = word.charAt(i) + newWord;
+  }
+  if (og === newWord)return true;
+  else return false;
+}
