ddcf9a2ea0df5c09625a5311fd7d80c60de2836c
Lucas Leifheit <43593024+lnardon@users.noreply.github.com>
2019-05-03 00:50:58 -0300

    Create randomNumber.js

---
+++ javascript/randomNumber.js
@@ -0,0 +1,7 @@
+//In this Algorithm you get a random number between 2 input values provided
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
